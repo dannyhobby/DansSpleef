b1a67092945ab16c8a3c72448b4e44574784e559
Daniel Hobson <danielhobby1994@gmail.com>
2013-05-19 15:25:45 +0100

    V 1.0.0

---
+++ src/com/github/danielhobby/dansspleef/Main.java
@@ -0,0 +1,163 @@
+package com.github.danielhobby.dansspleef;
+
+import java.util.HashSet;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public Location pos1 = null;
+	public Location pos2 = null;
+
+	public void onEnable() {
+	}
+
+	public void onDisable() {
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		Player player = (Player) sender;
+
+		if (cmd.getLabel().equals("test")) {
+			if ((this.pos1 == null) || (this.pos2 == null)) {
+				player.sendMessage("Please supply pos 1 and pos 2.");
+				return true;
+			}
+			World world = Bukkit.getWorld("world");
+			HashSet<Location> locations = new HashSet<Location>();
+
+			Location loc1 = this.pos1;
+			Location loc2 = this.pos2;
+
+			Boolean xpositive = Boolean.valueOf(true);
+			Boolean zpositive = Boolean.valueOf(true);
+			Boolean ypositive = Boolean.valueOf(true);
+
+			if (loc1.getX() > loc2.getX())
+				xpositive = Boolean.valueOf(false);
+			else {
+				xpositive = Boolean.valueOf(true);
+			}
+
+			if (loc1.getZ() > loc2.getZ())
+				zpositive = Boolean.valueOf(false);
+			else {
+				zpositive = Boolean.valueOf(true);
+			}
+
+			if (loc1.getY() > loc2.getY()) {
+				ypositive = Boolean.valueOf(false);
+			} else {
+				ypositive = Boolean.valueOf(true);
+			}
+			if ((xpositive.booleanValue()) && (zpositive.booleanValue())
+					&& (ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
+					for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
+						for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((xpositive.booleanValue()) && (zpositive.booleanValue())
+					&& (!ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
+					for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
+						for (int y = loc1.getBlockY(); y >= loc2.getBlockY(); y--)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((xpositive.booleanValue())
+					&& (!zpositive.booleanValue())
+					&& (ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
+					for (int z = loc1.getBlockZ(); z >= loc2.getBlockZ(); z--) {
+						for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((xpositive.booleanValue())
+					&& (!zpositive.booleanValue())
+					&& (!ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
+					for (int z = loc1.getBlockZ(); z >= loc2.getBlockZ(); z--) {
+						for (int y = loc1.getBlockY(); y >= loc2.getBlockY(); y--)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((!xpositive.booleanValue())
+					&& (zpositive.booleanValue()) && (ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x >= loc2.getBlockX(); x--) {
+					for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
+						for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((!xpositive.booleanValue())
+					&& (zpositive.booleanValue())
+					&& (!ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x >= loc2.getBlockX(); x--) {
+					for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
+						for (int y = loc1.getBlockY(); y >= loc2.getBlockY(); y--)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((!xpositive.booleanValue())
+					&& (!zpositive.booleanValue())
+					&& (ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x >= loc2.getBlockX(); x--) {
+					for (int z = loc1.getBlockZ(); z >= loc2.getBlockZ(); z--) {
+						for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++)
+							locations.add(new Location(world, x, y, z));
+					}
+				}
+			} else if ((!xpositive.booleanValue())
+					&& (!zpositive.booleanValue())
+					&& (!ypositive.booleanValue())) {
+				for (int x = loc1.getBlockX(); x >= loc2.getBlockX(); x--) {
+					for (int z = loc1.getBlockZ(); z >= loc2.getBlockZ(); z--) {
+						for (int y = loc1.getBlockY(); y >= loc2.getBlockY(); y--) {
+							locations.add(new Location(world, x, y, z));
+						}
+					}
+				}
+
+			}
+
+			int counter = 0;
+
+			for (Location loc : locations) {
+				Block floor = world.getBlockAt(loc);
+				if (floor.getType().equals(Material.AIR)) {
+					floor.setType(Material.SNOW_BLOCK);
+					counter++;
+				}
+			}
+
+			sender.sendMessage("Done, changed " + counter + " blocks.");
+
+			return true;
+		}
+		if ((cmd.getLabel().equals("spleef")) && (args[0].equals("pos1"))) {
+			this.pos1 = new Location(player.getWorld(), player.getLocation()
+					.getX(), player.getLocation().getY(), player.getLocation()
+					.getZ());
+			player.sendMessage("Added p1.");
+		} else if ((cmd.getLabel().equals("spleef"))
+				&& (args[0].equals("pos2"))) {
+			this.pos2 = new Location(player.getWorld(), player.getLocation()
+					.getX(), player.getLocation().getY(), player.getLocation()
+					.getZ());
+			player.sendMessage("Added p2.");
+		}
+
+		return false;
+	}
+}
